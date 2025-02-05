74f630aaace836a3229529ee2d31700684859dee
André Garcia Fattore <andrefattore33@gmail.com>
2025-02-05 20:13:00 -0300

    obtendo paramentros das rotas

--- estudo-rotas/src/app/app-routing.module.ts
+++ estudo-rotas/src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { CursosComponent } from './components/cursos/cursos.component';
+import { CursoDetalheComponent } from './components/curso-detalhe/curso-detalhe.component';
+
 
 const routes: Routes = [
   {path:'', component: HomeComponent},
   {path:'login', component: LoginComponent},
   {path:'cursos', component: CursosComponent},
+  {path:'curso/:id', component: CursoDetalheComponent},
 ];
 
 @NgModule({
--- estudo-rotas/src/app/app.module.ts
+++ estudo-rotas/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { CursosComponent } from './components/cursos/cursos.component';
+import { CursoDetalheComponent } from './components/curso-detalhe/curso-detalhe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    CursosComponent
+    CursosComponent,
+    CursoDetalheComponent
   ],
   imports: [
     BrowserModule,
